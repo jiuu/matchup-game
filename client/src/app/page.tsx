35a4b014b903ef6b2b4274510436100e042afd84
unknown <straisin@gmail.com>
2024-06-21 19:17:18 -0400

    feat: added analytics and speed insights

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,12 +3,19 @@ import { revalidatePath } from "next/cache";
 import { Quiz } from "@/components/Quiz.component";
 import mock from "@/mocks/matchups";
 import { QuizProvider } from "@/context/Quiz.context";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function Home() {
   return (
-    <QuizProvider>
-      <div>
-        <Quiz />
-      </div>
-    </QuizProvider>
+    <>
+      <QuizProvider>
+        <div>
+          <Quiz />
+        </div>
+      </QuizProvider>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
