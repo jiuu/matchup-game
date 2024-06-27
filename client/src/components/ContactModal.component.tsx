fee431745208ea3e86f5eedb2cb2e626156a319c
unknown <straisin@gmail.com>
2024-06-27 01:04:06 -0400

    feat: finished about modal

--- client/src/components/ContactModal.component.tsx
+++ client/src/components/ContactModal.component.tsx
@@ -1,17 +1,13 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Link, Typography } from "@mui/material";
 
-export const ContactModal = ({
-  handleClose,
-}: {
-  handleClose: () => void;
-}) => {
+export const ContactModal = ({ handleClose }: { handleClose: () => void }) => {
   const modalStyle = {
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width:{xs: "100%", sm:"40%"},
-    mx: {xs: '0rem', sm:"auto"},
+    width: { xs: "100%", sm: "40%" },
+    mx: { xs: "0rem", sm: "auto" },
 
     boxShadow: 24,
 
@@ -20,18 +16,34 @@ export const ContactModal = ({
     p: "2rem 3rem ",
     borderRadius: 4,
   };
-  console.log('hey')
+  console.log("hey");
 
   return (
     <div>
       <Box sx={modalStyle}>
-        
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          About
+        </Typography>
         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        Thanks for checking out my project! If you have any suggestions or inquiries, please shoot me an email at ___. Counterpick.me was created under Riot Games' "Legal Jibber Jabber" policy using assets owned by Riot Games.  Riot Games does not endorse or sponsor this project.
+          <Box sx={{ mb: 2 }}>
+            Thanks for checking out my project! If you have any suggestions,
+            inquiries, <span className="text-[8px]">or job offers 🙃,</span>{" "}
+            please shoot me an email at alexjwpak@gmail.com.
+          </Box>
+
+          <Box>
+            Counterpick.me was created under Riot Game&apos;s &quot;
+            <Link href="https://www.riotgames.com/en/legal">
+              Legal Jibber Jabber
+            </Link>
+            &quot; policy using assets owned by Riot Games. Riot Games does not
+            endorse or sponsor this project.
+          </Box>
         </Typography>
         <Box
           sx={{
-            
+            display: "flex",
+            justifyContent: "flex-end",
           }}
         >
           <Button
@@ -40,12 +52,12 @@ export const ContactModal = ({
             onClick={() => {
               handleClose();
             }}
-            sx={{ color: "white", mr: "1rem",display: "flex",
-            justifyContent: "flex-end", }}
+            sx={{
+              color: "white",
+            }}
           >
             Close
           </Button>
-          
         </Box>
       </Box>
     </div>
